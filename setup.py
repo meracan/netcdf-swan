bb97c76d13485ee19d988254f26ddcd3b26453d9
SamDLJ <sdlj@uvic.ca>
2020-02-19 12:01:31 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="netcdf-swan",
-    version="0.1",
+    version="0.0.1",
+    author="Samuel Johnson",
+    author_email="samdljohnson@gmail.com",
+    url="https://github.com/meracan/netcdf-swan",
     packages=find_packages('src', exclude=["tests", "tests.*"]),
     package_dir={'': 'src'},
     include_package_data=True,
     install_requires=[
-        'pandas',
-        'regex',
+        'scipy',
         'numpy',
+        'netcdf4',
     ],
+    python_requires='>=3.6',
 )

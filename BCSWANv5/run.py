e8fb502fb578ee020ae3a6a566a7b75ae07903ad
Julien.Cousineau <julien.cousineau@gmail.com>
2020-05-26 19:39:52 +0000

    Fix json files

--- BCSWANv5/run.py
+++ BCSWANv5/run.py
@@ -2,29 +2,18 @@ import os
 import json
 from netcdfswan import NetCDFSWAN
 
-swanFolder='../../s3/data'
-
-def test_getFiles():
-  print(NetCDFSWAN.getFiles(swanFolder))
-
-def test_printInfo():
-  NetCDFSWAN.printMatKeys(swanFolder)
-  NetCDFSWAN.printSpcShape(swanFolder)
-
-def test_getSpectralStationMetadata():
-  obj=NetCDFSWAN.getSpectralStationMetadata(swanFolder)
-  with open(os.path.join(swanFolder,"spectral.json"),"w+") as f:
-    json.dump(obj,f,indent=2)
-
-def test_preparingFunctions():
-  obj=NetCDFSWAN.prepareInputJSON('BCSWANv5.json',swanFolder)
-  with open(os.path.join(swanFolder,"intput.json"),"w+") as f:
-    json.dump(obj,f,indent=2)
-
-
 
 if __name__ == "__main__":
-  # test_getFiles()
-  # test_printInfo()
-  # test_getSpectralStationMetadata()
-  # test_preparingFunctions()
+  swanFolder='../../s3/data'
+  jsonFile='BCSWANv5.json'
+  input=NetCDFSWAN.prepareInputJSON(jsonFile,swanFolder,year=2004,month=1)
+  
+  swan=NetCDFSWAN(input)
+
+  # swan.s3.delete()
+  # print(swan.groups['s'].child)
+  # Write
+  # swan.uploadStatic(year=2004)
+  swan.uploadS()
+  # swan.uploadT()
+  # swan.uploadSpc()  
